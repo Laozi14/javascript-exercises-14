d17643024e85cfe687241877094358ba56c2c9c8
Laozi14 <rohit22rm@gmail.com>
2024-11-01 14:32:46 +0530

    Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    //iterate after 1st argument in array
+    for(let i = 1; i < arguments.length; i++){
+        while (array.includes(arguments[i])){
+            array.splice(array.indexOf(arguments[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
