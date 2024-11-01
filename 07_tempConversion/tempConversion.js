46b081ce29ae312ce441f61dc6eb9cab32ac541e
Laozi14 <rohit22rm@gmail.com>
2024-11-01 19:34:52 +0530

    Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let tempCel = Math.round(((far - 32)*5/9) * 10) /10;
+  return tempCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let tempFar = Math.round((cel * 9/5 + 32) * 10) / 10;
+  return tempFar; 
 };
 
 // Do not edit below this line
