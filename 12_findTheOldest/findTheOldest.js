86627f7cfde970cc8f219cc896cabfaf9d3b05cd
Laozi14 <rohit22rm@gmail.com>
2024-11-12 15:17:38 +0530

    Exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        // Calculate the age at death or, if still alive, the age up to the current year
+        const currentYear = new Date().getFullYear();
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        
+        // Update the oldest person if the current person's age is greater
+        return personAge > oldestAge ? person : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
