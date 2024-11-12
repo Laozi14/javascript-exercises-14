7e64b59502762ba4b7b1a719f59e825f63deea6c
Laozi14 <rohit22rm@gmail.com>
2024-11-12 14:11:45 +0530

    Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = str => {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');    
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = n => {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    let [a, b] = [1, 1];
+    for (let i = 2; i < n; i++) [a, b] = [b, a + b];
+    return b;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
